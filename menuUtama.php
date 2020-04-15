1a10da8d1db0ac6ef11c22e79f7ffb5e7740580b
Muhammad Dio Syahputra <diosyahputra98765@gmail.com>
2020-04-15 12:44:55 +0800

    penambahan crud

--- menuUtama.php
+++ menuUtama.php
@@ -12,6 +12,9 @@ if (!$_SESSION["is_login"] === TRUE) {
 
 <head>
     <meta charset="UTF-8">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 
@@ -20,13 +23,21 @@ if (!$_SESSION["is_login"] === TRUE) {
 
     </header>
     <main>
-        <form action="" method="POST">
-            <label>Name</label>
-            <input type="text" name="name" value="Enter your name">
-            <label>Alamat</label>
-            <input type="text" name="location" value="Enter your location">
-            <button type="submit" name="save">Simpan</button>
-        </form>
+        <div class="row justify-content-center">
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" name="name" class="form-control" value="Enter your name">
+                </div>
+                <div class="form-group">
+                    <label>Alamat</label>
+                    <input type="text" name="location" class="form-control" value="Enter your location">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary" name="save">Simpan</button>
+                </div>
+            </form>
+        </div>
     </main>
     <footer>
 
